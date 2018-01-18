b1556e70177c3983e5559fa5a6447a6508b5e5e8
Nathan Mustaki <feydaykyn@gmail.com>
2018-01-18 11:34:39 +0100

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -e
 
-# Only needed once
-# python setup.py register -r https://testpypi.python.org/pypi
-# python setup.py register -r https://pypi.python.org/pypi
+echo "Remember to update version in :"
+echo "    - debinterface/__init__.py"
+echo "    - setup.py"
 
 # Test
-python setup.py test
+tox
 
 # Cleanup
 rm -rf debinterface.egg-info
@@ -22,9 +23,3 @@ python setup.py bdist_wheel  # Not universal
 # Deploy with gpg
 twine upload dist/* -r pypitest --sign --skip-existing
 twine upload dist/* --sign
-
-# Deploy legacy version
-# Upload to test PyPi
-# python setup.py sdist upload -r https://testpypi.python.org/pypi
-# Upload to prod PyPi
-# python setup.py sdist upload
