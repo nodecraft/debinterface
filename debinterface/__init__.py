d99dfa94a42d70900e31c36023602bea3e5efdfb
Nathan Mustaki <feydaykyn@gmail.com>
2018-01-18 11:31:18 +0100

    Bump forgotten version to 3.2

--- debinterface/__init__.py
+++ debinterface/__init__.py
@@ -9,7 +9,7 @@ from .interfaces import Interfaces
 from .interfacesReader import InterfacesReader
 from .interfacesWriter import InterfacesWriter
 
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 
 __all__ = [
     'NetworkAdapter',
